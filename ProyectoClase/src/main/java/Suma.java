eaadca803527550b757fa9c34e52229243229879
Albertogf98 <alberto1998adoratrices@gmail.com>
2020-03-10 13:49:47 +0100

    XML SNAP 11

--- ProyectoClase/src/main/java/Suma.java
+++ ProyectoClase/src/main/java/Suma.java
@@ -1,5 +1,9 @@
 public class Suma {
     public static int sumar(int a, int b) {
-        return a+b;
+        return a + b;
+    }
+
+    public static double sumarD(double a, double b) {
+        return a + b;
     }
 }
